f786c36802b051d69fd0618c659a2007a9a672b0
wrlpeake <wrlpeake@gmail.com>
2022-10-24 12:05:28 +0100

    rubocop

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'player'
+require_relative "player"
 
 class HumanPlayer < Player
   attr_accessor :marker
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require_relative '../lib/computer_player'
+require_relative "../lib/computer_player"
 
 RSpec.describe ComputerPlayer do
-  it 'should initialize a new computer player with the marker O' do
-    @computer_player = ComputerPlayer.new('O')
+  it "should initialize a new computer player with the marker O" do
+    @computer_player = ComputerPlayer.new("O")
 
-    expect(@computer_player.marker).to eq 'O'
+    expect(@computer_player.marker).to eq "O"
   end
 
-  it 'should get the board position for its move' do
-    @computer_player = ComputerPlayer.new('X')
+  it "should get the board position for its move" do
+    @computer_player = ComputerPlayer.new("X")
     first_spot_available = 3
 
     expect(@computer_player.get_move(first_spot_available)).to eq first_spot_available
