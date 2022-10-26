3f1781f73df8f34c793f20b429f46f33d7c86959
wrlpeake <wrlpeake@gmail.com>
2022-10-26 10:56:48 +0100

    Removing multiple `describe` blocks after resolving linting issue

--- spec/tic_tac_toe_wp_spec.rb
+++ spec/tic_tac_toe_wp_spec.rb
@@ -19,11 +19,7 @@ RSpec.describe TicTacToeWP::GameLogic do
 
     expect(@tic_tac_toe_wp.is_there_a_winner?).to be true
   end
-end
-RSpec.describe TicTacToeWP::GameLogic do
-  before(:each) do
-    @tic_tac_toe_wp = TicTacToeWP::GameLogic.new("X", "O")
-  end
+
   it "should check for three in a row horizontally" do
     marker = "O"
     @tic_tac_toe_wp.mark_game_board(marker, 7)
@@ -41,11 +37,7 @@ RSpec.describe TicTacToeWP::GameLogic do
 
     expect(@tic_tac_toe_wp.check_board_vertically?).to be true
   end
-end
-RSpec.describe TicTacToeWP::GameLogic do
-  before(:each) do
-    @tic_tac_toe_wp = TicTacToeWP::GameLogic.new("X", "O")
-  end
+
   it "should check the board for three in a row diagonally" do
     marker = "X"
     @tic_tac_toe_wp.mark_game_board(marker, 3)
@@ -63,11 +55,7 @@ RSpec.describe TicTacToeWP::GameLogic do
 
     expect(@tic_tac_toe_wp.check_diagonal_left_to_right?).to be true
   end
-end
-RSpec.describe TicTacToeWP::GameLogic do
-  before(:each) do
-    @tic_tac_toe_wp = TicTacToeWP::GameLogic.new("X", "O")
-  end
+
   it "should check for three in a row diagonally from top right to bottom left" do
     marker = "O"
     @tic_tac_toe_wp.mark_game_board(marker, 3)
@@ -76,11 +64,7 @@ RSpec.describe TicTacToeWP::GameLogic do
 
     expect(@tic_tac_toe_wp.check_diagonal_right_to_left?).to be true
   end
-end
-RSpec.describe TicTacToeWP::GameLogic do
-  before(:each) do
-    @tic_tac_toe_wp = TicTacToeWP::GameLogic.new("X", "O")
-  end
+
   it "should recognise when a game has been tied" do
     player_one_marker = "X"
     player_two_marker = "O"
@@ -96,11 +80,7 @@ RSpec.describe TicTacToeWP::GameLogic do
 
     expect(@tic_tac_toe_wp.get_available_positions).to eql []
   end
-end
-RSpec.describe TicTacToeWP::GameLogic do
-  before(:each) do
-    @tic_tac_toe_wp = TicTacToeWP::GameLogic.new("X", "O")
-  end
+
   it "validate_human_player_selection() should return 0 if the human player selection is valid" do
     position = 4
 
@@ -116,11 +96,7 @@ RSpec.describe TicTacToeWP::GameLogic do
 
     expect(validation_code).to eql 1
   end
-end
-RSpec.describe TicTacToeWP::GameLogic do
-  before(:each) do
-    @tic_tac_toe_wp = TicTacToeWP::GameLogic.new("X", "O")
-  end
+
   it "validate_human_player_selection() should return 2 if the human player selection has already been chosen" do
     @tic_tac_toe_wp.mark_game_board("O", 5)
 
@@ -137,11 +113,7 @@ RSpec.describe TicTacToeWP::GameLogic do
     validation_code = @tic_tac_toe_wp.validate_game_type_selection(game_type_selection)
     expect(validation_code).to eql game_type_selection
   end
-end
-RSpec.describe TicTacToeWP::GameLogic do
-  before(:each) do
-    @tic_tac_toe_wp = TicTacToeWP::GameLogic.new("X", "O")
-  end
+
   it "validate_game_type_selection() should return 0 if the selection is anything other than a number between 1-4" do
     game_type_selection = 6
 
@@ -156,11 +128,7 @@ RSpec.describe TicTacToeWP::GameLogic do
 
     expect(@tic_tac_toe_wp.get_available_positions).to eql [1, 2, 3, 4, 5, 6, 7, 8]
   end
-end
-RSpec.describe TicTacToeWP::GameLogic do
-  before(:each) do
-    @tic_tac_toe_wp = TicTacToeWP::GameLogic.new("X", "O")
-  end
+
   it "should return the first available spot on the board" do
     expect(@tic_tac_toe_wp.get_first_spot_available).to eql 1
   end
